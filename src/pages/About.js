becdd227911da3a08d481b7ab8641c587bff98b2
John Cardenas <jhoncardenaspinto@gmail.com>
2020-10-01 22:03:09 -0500

    add the new page About the project

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+  const view = `
+    <div class="About">
+      <h2>About</h2>
+      <p>Rick and Morty Single Page Application using <a href="https://rickandmortyapi.com/" target="_blank">The Rick and Morty API</a></p>
+    </div>
+  `;
+  return view;
+};
+
+export default About;
