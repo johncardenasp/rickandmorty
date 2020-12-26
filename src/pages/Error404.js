aac414c64782b6504934e68b77b012da8d00bfe7
John Cardenas <jhoncardenaspinto@gmail.com>
2020-12-26 17:09:09 -0500

    improving UI and UX of Error404 page

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,14 +1,30 @@
 const Error404 = () => {
+  let header = document.querySelector("header");
+
   const view = `
   <div class="Error404">
-<!-- <figure>
-      <img 
-      src="../src/images/rick-and-morty.png"
-      >
-    </figure>-->
-    <h2>Error 404 - Page not found</h2>
+    <h3>Error 404 - Page not found</h3>
+    <p>
+      This is not your dimension. 
+      <br /> 
+      Please click 
+      <a href="/">
+        here to back home
+      </a>
+      🏠
+    </p>
+    <figure>
+      <img src="../src/images/rick-and-morty.png">
+    </figure>
     </div>
   `;
+
+  if (Error404) {
+    header.classList.add("Header-hidden");
+  } else {
+    header.classList.remove("Header-hidden");
+  }
+
   return view;
 };
 
